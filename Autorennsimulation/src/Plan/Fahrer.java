af88bb2848dee0b5060cd38495665aa69f761e59
N8licht <jenssan1991@gmail.com>
2017-05-15 15:40:47 +0200

    Update Fahrer.java

--- Autorennsimulation/src/Plan/Fahrer.java
+++ Autorennsimulation/src/Plan/Fahrer.java
@@ -1,5 +1,29 @@
 package Plan;
 
-public class Fahrer {
+public class Fahrer extends Teammietglieder {
+int pilotenNr;
+int pilotenSiege;
+boolean zugeordnet;
+int gehalt;
+int bonus;
+int saisonRennen;
+int gewonnenerennen;
+int saisongehaltfahrer;
 
+Fahrer(){
+}
+Fahrer(int pilotenNr, int pilotensiege, boolean zugeordnet){
+	this.pilotenNr=pilotenNr;
+	this.pilotenSiege=pilotensiege;
+	this.zugeordnet=zugeordnet;
+}
+
+public static int saisongehaltfahrer( int gehalt, int bonus, int gewonnenerennen, int saisonRennen){
+ {
+	int  saisongehaltfahrer = gehalt*saisonRennen+(bonus*gewonnenerennen);
+
+
+System.out.println("Das Saisongehalt des Fahrers beträgt" saisongehaltfahrer);
+}
+}
 }
